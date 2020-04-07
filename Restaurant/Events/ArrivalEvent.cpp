bbec033d035fd1bd39f89784c877e63c526a4419
Carolinemagdy <caroline.magdy12@yahoo.com>
2020-04-07 03:26:12 +0200

    caroline 2nd commiy

--- Restaurant/Events/ArrivalEvent.cpp
+++ Restaurant/Events/ArrivalEvent.cpp
@@ -18,7 +18,19 @@ void ArrivalEvent::Execute(Restaurant* pRest)
 	//This function should create an order and fills its data 
 	// Then adds it to normal, vegan, or VIP order lists that you will create in phase1
 
-	
+	Order* pOrd = new Order(OrderID,OrdType);
+	if (pOrd->GetType()==TYPE_NRM)
+	{
+		pRest->Addtowaitingnormal(pOrd);
+	}
+	if (pOrd->GetType()==TYPE_VGAN)
+	{
+		pRest->Addtowaitingvegan(pOrd);
+	}
+	if (pOrd->GetType()==TYPE_VIP)
+	{
+		pRest->Addtowaitingvip(pOrd);
+	}
 	
 	///For the sake of demo, this function will just create an order and add it to DemoQueue
 	///Remove the next code lines in phases 1&2
